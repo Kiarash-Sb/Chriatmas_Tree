f57883746175c7dc7ba0a836ccc315533290f503
Kiarash Siahbandi <kiarashsiahbandi@gmail.com>
2022-05-02 20:37:01 +0430

    Add files via upload
    <Special program used for Christmas event.>
    Just enter a number(low count is better) and see it yourself.
    Easy and fun useable for entertainment.

---
+++ Christmas_Tree.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int rows, i, j, space;
+	cout << "Enter number of rows : " << endl;
+	cin >> rows;
+	for (i = 1; i <= rows; i++)
+		{for (space = i; space < rows; space++)
+			{cout << " ";
+				}
+		for (j = 1;j <= (2 * i - 1); j++)
+			{cout << "*";
+				}
+		cout << endl;	
+		}
+}
